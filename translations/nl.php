646ab955a767ac726e568df61db74da508c6472f
Michael Dekker <git@michaeldekker.nl>
2016-07-11 20:18:02 +0200

    Add Dutch translation

---
+++ translations/nl.php
@@ -0,0 +1,28 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{twittercard}prestashop>twittercard_321c45db6dfa86aed68adaba3962020b'] = 'Twitterkaart met grote afbeelding';
+$_MODULE['<{twittercard}prestashop>twittercard_0a17ea6281134d0c0978b7b419c0cba1'] = 'Voegt een Twitterkaart met een grote afbeelding toe aan uw site';
+$_MODULE['<{twittercard}prestashop>twittercard_c9cc8cce247e49bae79f15173ce97354'] = 'Opslaan';
+$_MODULE['<{twittercard}prestashop>twittercard_630f6dc397fe74e52d5189e2c80f282b'] = 'Terug naar lijst';
+$_MODULE['<{twittercard}prestashop>twittercard_523ab26457167e3a286cf62c301641a9'] = 'Twitterkaart-configuratie';
+$_MODULE['<{twittercard}prestashop>twittercard_95eec51b8dbfc3be19a025781e5c52fc'] = 'Je kunt je twitterkaarten %shier%s testen.';
+$_MODULE['<{twittercard}prestashop>twittercard_f5f0ec7f83db896649cad1229aa983e4'] = 'Twitternaam (bijv. @strikehawkecomm)';
+$_MODULE['<{twittercard}prestashop>twittercard_bfeb95b6fff23fe16dd1dd231cadb451'] = 'Homepage-titel';
+$_MODULE['<{twittercard}prestashop>twittercard_962a046fc15e89bcc4c3fb24060a2f09'] = 'Homepage-beschrijving (limiet: 200 karakters)';
+$_MODULE['<{twittercard}prestashop>twittercard_c7390726ae846a1c0f208dd6d6ae52a4'] = 'Homepage-afbeelding';
+$_MODULE['<{twittercard}prestashop>twittercard_93cba07454f06a4a960172bbd6e2a435'] = 'Ja';
+$_MODULE['<{twittercard}prestashop>twittercard_bafd7322c6e97d25b6299b5d6fe8920b'] = 'Nee';
+$_MODULE['<{twittercard}prestashop>twittercard_3cdc0be803d80843cf4ef83202eba8b3'] = 'Voer de URL van het logo in:';
+$_MODULE['<{twittercard}prestashop>twittercard_a616bb9537b0ea326d8901ec927f6a2f'] = 'Productafbeeldingen opnieuw genereren';
+$_MODULE['<{twittercard}prestashop>twittercard_feeaca97c3191e53347e4fd0679b4a18'] = 'Vlak na het installeren van deze module is een nieuw afbeeldingstype geconfigureerd (twitter_default) om afbeeldingen te optimaliseren voor de Twitterkaart. Om bestaande productafbeeldingen te optimaliseren moeten deze eerst opnieuw gegenereerd worden. Met deze optie kun je dat gemakkelijk doen.';
+$_MODULE['<{twittercard}prestashop>twittercard_4795c891184662f0d99ff8a45cc484ea'] = 'Ik wil graag productafbeeldingen opnieuw genereren.';
+$_MODULE['<{twittercard}prestashop>twittercard_b2326c3a3498ee50c8120fbaede6eeed'] = 'Opnieuw genereren';
+$_MODULE['<{twittercard}prestashop>twittercard_6454f33dab3065952168af9fcba650f4'] = '{{menu display name}}';
+$_MODULE['<{twittercard}prestashop>twittercard_5b776dc853fc1efe91f3ddd2b2fe495b'] = '{{menu description}}';
+$_MODULE['<{twittercard}prestashop>twittercard_f4f70727dc34561dfde1a3c529b6205c'] = 'Instellingen';
+$_MODULE['<{twittercard}prestashop>twittercard_1a11db3d2781e8ee1401eab17b296b6a'] = 'Module-instellingen';
+$_MODULE['<{twittercard}prestashop>twittercard_6a26f548831e6a8c26bfbbd9f6ec61e0'] = 'Help';
+$_MODULE['<{twittercard}prestashop>twittercard_082c7196a2d6e0115c4a866ebf8d6c8f'] = 'Instellingen met success bijgewerkt';
+$_MODULE['<{twittercard}prestashop>twittercard_e98f7437066e499e56737c85f2912596'] = 'Er is een probleem opgetreden bij het bijwerken van de configuratie ';
